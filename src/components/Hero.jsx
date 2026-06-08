34a244b5948fb83f2f0fc82d424b0be65a2bd6d7
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2026-06-08 17:35:00 -0300

    WIP: Products

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,8 @@ import { useRef } from "react";
 export default function Hero() {
     const section1Ref = useRef(null);
     const section2Ref = useRef(null);
-     
+    const section3Ref = useRef(null);
+          
     const { scrollYProgress: progressSection1 } = useScroll({
         target: section1Ref,
         offset: ["start end", "end start"] 
@@ -15,19 +16,23 @@ export default function Hero() {
         offset: ["start end", "end start"] 
     });
 
+    const { scrollYProgress: progressSection3 } = useScroll({
+        target: section2Ref,
+        offset: ["start end", "end start"] 
+    });
 
     const opacitySection1 = useTransform(progressSection1, [0.5, 0.54], [0, 1]);
-    const opacitySection2 = useTransform(progressSection2, [0, 0.3], [0, 1]);
+    const opacitySection2 = useTransform(progressSection2, [0.3, 0.5], [0, 1]);
+    const opacitySection3 = useTransform(progressSection3, [0.3, 0.5], [0, 1]);
+
     const rawY1 = useTransform(progressSection1,  [0.5,0.52], ["-100vh", "100vh"]);
     const rawX2 = useTransform(progressSection2, [0, 0.4,0.6 , 1], ["100vw", "0vw","0vw" ,"-100vw"]);
+    const rawX3 = useTransform(progressSection3, [0, 0.4,0.6 , 1], ["-100vw", "0vw","0vw" ,"100vw"]);
 
     const ySection1 = useSpring(rawY1, { stiffness: 60, damping: 20 });
     const xSection2 = useSpring(rawX2, { stiffness: 60, damping: 20 });
-   
-    useMotionValueEvent(progressSection1, "change", (latestValue) => {
-        console.log("Section 1 Vertical Progress:", latestValue);
-    });
-
+    const xSection3 = useSpring(rawX3, { stiffness: 60, damping: 20 });
+     
     return (
         <section id="home" className="w-full flex justify-center items-center flex-col">
             <div ref={section1Ref} className="bg-slate-900 bg-[url('/hero.png')] bg-cover w-full h-[150vh] flex justify-center items-center overflow-hidden">
@@ -38,12 +43,17 @@ export default function Hero() {
                 </div>
             </div>
 
-            <div ref={section2Ref} className="bg-slate-300 w-full h-[120vh] flex justify-end items-center overflow-hidden">
+            <div ref={section2Ref} className="bg-slate-300 py-10 w-full h-full flex justify-end items-center overflow-hidden">
                 <motion.p style={{ x: xSection2, opacity: opacitySection2 }} className="will-change-transform font-dela-gothic text-right w-[40%] mr-[10%] sm:text-2xl text-sm text-slate-800"> 
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eos obcaecati architecto facere non molestias soluta odit! Nam non, praesentium necessitatibus enim tempora dolorem mollitia magni itaque iusto, quis delectus.
                 </motion.p>
             </div>
-            
+    
+            <div ref={section3Ref} className="bg-slate-300 py-10 w-full h-full flex justify-start items-center overflow-hidden">
+                <motion.p style={{ x: xSection3, opacity: opacitySection3 }} className="will-change-transform font-dela-gothic text-left w-[40%] ml-[10%] sm:text-2xl text-sm text-slate-800"> 
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eos obcaecati architecto facere non molestias soluta odit! Nam non, praesentium necessitatibus enim tempora dolorem mollitia magni itaque iusto, quis delectus.
+                </motion.p>
+            </div>        
         </section>
     );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,23 @@
 import { useRef } from "react";
-
+import { motion,useScroll,useSpring,useTransform } from "motion/react";
 export default function Products() {
-    const section1Ref = useRef(null);
-    return <div className="w-full h-screen flex justify-center items-center bg-slate-400">
-        <h1>Products</h1>
-
+  const headerRef = useRef(null);
+  const { scrollYProgress: progressHeader } = useScroll({
+    target: headerRef,
+    offset: ["start end", "end start"],
+  });
+  const opacityHeader = useTransform(progressHeader, [0.3, 0.5], [0, 1]);
+  const rawY1 = useTransform(progressHeader, [0, 1], ["0vh", "15vh"]);
+  const yHeader = useSpring(rawY1, { stiffness: 60, damping: 20 });
+  return (
+    <div className="w-full h-screen flex justify-center items-start bg-slate-400">
+      <motion.h1
+        style={{ y: yHeader, opacity: opacityHeader }}
+        ref={headerRef}
+        className="text-xl sm:text-4xl "
+      >
+        Products
+      </motion.h1>
     </div>
-
+  );
 }
