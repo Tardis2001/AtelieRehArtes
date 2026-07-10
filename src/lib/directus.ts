d19d44b6f5cb5935a3e3ecc9c4c245f6a0b01e5a
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2026-07-10 15:20:29 -0300

    feat: directus integration

---
+++ src/lib/directus.ts
@@ -0,0 +1,36 @@
+import { createDirectus, rest } from '@directus/sdk';
+
+export interface Produtos {
+  id: number;
+  nome: string;
+  category: string;
+  description: string;
+  price: string;
+  image: string; 
+}
+
+export interface Sobre {
+  title: string;
+  bioParagraph1: string;
+  bioParagraph2: string;
+  foto_perfil: string;
+}
+export interface Global {
+   title: string;
+   description: string; 
+}
+export interface Hero {
+   title: string;
+   subtitle: string; 
+}
+
+interface Schema {
+  Produtos: Produtos[];
+  Sobre: Sobre;
+  Global: Global;
+  Hero: Hero;
+}
+
+const DIRECTUS_URL = import.meta.env.PUBLIC_DIRECTUS_URL;
+
+export const directus = createDirectus<Schema>(DIRECTUS_URL).with(rest());
