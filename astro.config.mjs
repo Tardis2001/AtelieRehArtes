5fb94315c72e1549c3ed1c68ef27e14107039c3e
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2026-07-11 09:29:54 -0300

    add vercel adapter; SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-
+  output: 'server',
+   adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
