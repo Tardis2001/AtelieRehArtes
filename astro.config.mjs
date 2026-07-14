2b4b507e356e77e43b52f08ab7100b98ebc645b5
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2026-07-14 15:39:40 -0300

    del: directus integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
